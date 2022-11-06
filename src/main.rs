3d9b6b78b7d1fd04d0d3bf5f83dad673384b6e67
Knoxius NomNom <knox.dev@proton.me>
2022-11-06 21:17:07 +0545

    v0.1.2 [BufReader .^_.^]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use console::style;
 use std::io;
+use clap::Parser;
+
+// Error E0277 =P
+#[derive(Debug)]
+// Search for a patern in a file and display the lines that contain it.
+#[derive(Parser)]
+    struct Aidk {
+    // The patern to look for
+    pattern: String,
+    // The path to the file to read
+    path: std::path::PathBuf,
+}
 
 fn main() {
+    // Something something
+    let args = Aidk::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
     // Welcome screen
     println!("{}", style("Welcome to hell uvu\n").bold().green());
     // Variable to store the word
@@ -16,4 +31,10 @@ fn main() {
     println!("{}", style(word).yellow());
     // End screen
     println!("{}", style("Enjoy you're stay <3\n").bold().red());
+    // Print out args and pattern
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
